2c468be2bd7a62d0f282d37382a0c4d062e7b670
Emmanuel Akins <Akins1412>
2024-01-11 02:38:09 -0800

    Python

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,5 @@
 def update_dictionary(a_dictionary, key, value):
     "replaces or adds key/value in a dictionary"
 
-
-	a_dictionary[key] = value
-	return(a_dictionary)
+    a_dictionary[key] = value
+    return (a_dictionary)
