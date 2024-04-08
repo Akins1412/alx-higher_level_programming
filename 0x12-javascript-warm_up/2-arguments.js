995b1d323a8c89c68d77becab49d4ec6ea2c72ea
Emmanuel <Akins1412>
2024-04-08 13:27:33 -0700

    warm up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let argvLen = process.argv.length;
+if (argvLen === 2) {
+  console.log("No argument");
+} else if (argvLen === 3) {
+  console.log("Argument found");
+} else {
+  console.log("Argument found");
+}
