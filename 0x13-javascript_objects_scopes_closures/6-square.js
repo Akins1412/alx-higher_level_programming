c736baacda41a74779a999080546a8b156690430
Emmanuel <Akins1412>
2024-04-10 07:26:30 -0700

    initials

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,9 +5,10 @@ class Square extends Squares {
     if (c === undefined) {
       c = "X";
     }
-    for (i = 0; i < this.height; i++) {
+    for (let i = 0; i < this.height; i++) {
       console.log("c".repeat(this.width));
     }
   }
 }
+
 module.exports = Square;
