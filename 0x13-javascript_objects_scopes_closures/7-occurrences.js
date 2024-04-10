09aa934c87979800f7e0095913871e77fb7d2b73
Emmanuel <Akins1412>
2024-04-10 13:18:32 -0700

    initials

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let nbOccurences = 0;
-  for (let i = 0; i < nbOccurences; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
       nOccurrences++;
     }
