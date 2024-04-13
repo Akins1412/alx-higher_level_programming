725e51792a27b1b9baec3f6c964e10ee46b6d406
Emmanuel Akins <Akins1412>
2024-04-13 11:47:59 -0700

    initials

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
                          psswrd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE BINARY NAME 'N%' 
-                   ORDER BY state.id""")
+                   ORDER BY states.id""")
     res = cur.fetchall()
     for result in res:
         print(result)
