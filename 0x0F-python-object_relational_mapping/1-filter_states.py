7245f365e8e6bbdcea499e3a250dba465123a24e
Emmanuel Akins <Akins1412>
2024-04-13 11:51:37 -0700

    initials

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,7 +3,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MYSQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          psswrd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute = ("""SELECT * FROM states WHERE BINARY NAME 'N%' 
