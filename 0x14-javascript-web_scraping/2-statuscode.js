1774642fe147068922412b2f2e1d45df7cfcd881
Findme <Akins1412>
2024-05-22 10:23:12 -0700

    initials

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request';
 
 request.get(proces.argv[2], (err, response) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log("code: " + response.statusCode);
+  console.log('code: ' + response.statusCode);
 });
